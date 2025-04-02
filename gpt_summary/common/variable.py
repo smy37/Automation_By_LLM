2579ea28dada58553743440e2c8f5f8b854f3240
smy37 <seongminyang37@gmail.com>
2025-04-02 21:44:18 +0900

    <수정>이미 있는 날짜의 파일은 추가하지 않도록 수정

--- gpt_summary/common/variable.py
+++ gpt_summary/common/variable.py
@@ -1,2 +1,3 @@
 DEFAULT_LLM_MODEL = "gpt-4o"
 MAX_PIXELS = 89478000
+EMBEDDING_MODEL = "text-embedding-3-large"
--- gpt_summary/main.py
+++ gpt_summary/main.py
@@ -9,10 +9,6 @@ from common.openai_api import ask_gpt
 from common import prompt_lib
 from common.multi_thread import process_multi_thread
 from pydantic import BaseModel
-class output_format(BaseModel):
-    markdown_summary: str
-
-EMBEDDING_MODEL = "text-embedding-3-large"
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 def get_korea_time(utc_time):
@@ -28,50 +24,63 @@ def count_gpt4o_tokens(text: str) -> int:
     encoding = tiktoken.get_encoding("cl100k_base")
     return len(encoding.encode(text))
 
-data_path = BASE_DIR + r"/conversations.json"
-json_data = json.load(open(data_path))
-print(len(json_data))
-
-data_by_time = {}
-for i in range(len(json_data)): ## 대화방 단위
-    mapping = json_data[i].get("mapping")
-    create_time = json_data[i].get("create_time")
-
-    date_str= get_korea_time(create_time)
-    if date_str not in data_by_time:
-        data_by_time[date_str] = []
-    temp = ""
-    for k in mapping:   ## 대화말 단위
-        if mapping[k].get("message"):
-            role = mapping[k].get("message").get("author")["role"]
-            if mapping[k].get("message").get("content").get("parts"):
-                msg = mapping[k].get("message").get("content").get("parts")[0]
-                if len(msg) >0 and msg:
-                    temp += f"{role}: {msg}\n\n"
-            else:
-                txt_msg = mapping[k].get("message").get("content").get("text")
-                temp += f"{role}: {txt_msg}\n\n"
-    data_by_time[date_str].append(temp)       
-
+class output_format(BaseModel):
+    markdown_summary: str
 
 def summary_process(temp_data):
     resp = ask_gpt(temp_data, prompt_lib.summary_prompt, output_format).markdown_summary
     return resp
 
-write_path = "./result"
-sum_token = 0
-for day in data_by_time:
-    f_name = day.replace("-", "_")+".md"
-    temp = ""
-    temp_datas = []
-    for chat_room in data_by_time[day]:
-        chat_room = chat_room.replace('<|endoftext|>', '')
-        token_num = count_gpt4o_tokens(chat_room)
-        sum_token += token_num
-        temp_datas.append(chat_room)
-    after_data = process_multi_thread(temp_datas, summary_process)
 
-    with open(os.path.join(write_path, f_name), 'w', encoding='utf-8') as wr:
-        wr.write("\n\n".join(after_data))
+if __name__ == "__main__":
+    data_path = BASE_DIR + r"/conversations.json"
+    json_data = json.load(open(data_path))
+    print(len(json_data))
 
-print("예상가격(달러):", sum_token/1000000*5)
+    data_by_time = {}
+    for i in range(len(json_data)): ## 대화방 단위
+        mapping = json_data[i].get("mapping")
+        create_time = json_data[i].get("create_time")
+
+        date_str= get_korea_time(create_time)
+        if date_str not in data_by_time:
+            data_by_time[date_str] = []
+        temp = ""
+        for k in mapping:   ## 대화말 단위
+            if mapping[k].get("message"):
+                role = mapping[k].get("message").get("author")["role"]
+                if mapping[k].get("message").get("content").get("parts"):
+                    msg = mapping[k].get("message").get("content").get("parts")[0]
+                    if len(msg) >0 and msg:
+                        temp += f"{role}: {msg}\n\n"
+                else:
+                    txt_msg = mapping[k].get("message").get("content").get("text")
+                    temp += f"{role}: {txt_msg}\n\n"
+        data_by_time[date_str].append(temp)
+
+
+    write_path = "./result"
+    sum_token = 0
+    b_multi_thread = False
+
+    for day in data_by_time:
+        if day.replace("-", "_")+".md" in os.listdir(write_path):   ## 이미 있는 날짜의 파일이면 다시 생성하지 않음.
+            continue
+        f_name = day.replace("-", "_")+".md"
+        temp = []
+        temp_datas = []
+        for chat_room in data_by_time[day]:
+            chat_room = chat_room.replace('<|endoftext|>', '')
+            token_num = count_gpt4o_tokens(chat_room)
+            sum_token += token_num
+            temp_datas.append(chat_room)
+            if not b_multi_thread:
+                temp.append(ask_gpt(chat_room, prompt_lib.summary_prompt, output_format).markdown_summary)
+        if b_multi_thread:
+            after_data = process_multi_thread(temp_datas, summary_process)
+        else:
+            after_data = temp
+        with open(os.path.join(write_path, f_name), 'w', encoding='utf-8') as wr:
+            wr.write("\n\n".join(after_data))
+
+    print("예상가격(달러):", sum_token/1000000*5)
